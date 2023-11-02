0ad220e1d315bf1b385cac5a9185c8f3af8c3cc3
madara1319 <mateusz.bik@op.pl>
2023-11-02 17:54:18 +0100

    Added task about improving finding max value in array

--- boredJS.js
+++ boredJS.js
@@ -1,11 +1,50 @@
-console.log('Test');
-
-
-const star='*';
-let multiStar=[];
-for (let i=1; i<=5; i++)
-{
-	multiStar.push(' ');
-	multiStar.push(star)
-	console.log(multiStar);
+//This function in big O Notation classifies under O(N2) efficiency category
+function findGreatestValueInArray(array) {
+  let isIValueTheGreatest;
+  let steps = 0;
+  for (let i = 0; i < array.length; i++) {
+    isIValueTheGreatest = true;
+    for (let j = 0; j < array.length; j++) {
+      steps++;
+      if (array[j] > array[i]) {
+        isIValueTheGreatest = false;
+      }
+    }
+    if (isIValueTheGreatest) {
+      console.log(`steps number is ${steps}`);
+      return array[i];
+    }
+  }
 }
+//Here i managed to write solution with efficiency that would classify as O(N)
+
+function findTheGreatestValueInArrayButFaster(array) {
+  let valuesAsIndexesArray = [];
+  steps = 0;
+  for (let i = 0; i < array.length; i++) {
+    steps++;
+    valuesAsIndexesArray[array[i]] = 1;
+  }
+
+  console.log(`steps number is ${steps}`);
+  return valuesAsIndexesArray.length - 1;
+}
+
+function findGreatestValueInArrayOptimal(array) {
+  let greatestValue = array[0];
+  let steps = 0;
+  for (let i = 0; i < array.length; i++) {
+    steps++;
+    if (array[i] > greatestValue) {
+      greatestValue = array[i];
+    }
+  }
+
+  console.log(`steps number is ${steps}`);
+  return greatestValue;
+}
+
+// console.log(findGreatestValueInArray([1, 5, 6, 2, 7]));
+// console.log(findTheGreatestValueInArrayButFaster([1, 5, 23, 10, 2]));
+
+console.log(findGreatestValueInArrayOptimal([1, 5, 23, 10, 2]));
